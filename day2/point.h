2cd3352373627131432686604f45e331ef692b69
Stephen Houser <stephenhouser@gmail.com>
2025-02-09 14:41:03 -0500

    day 2 part 1

---
+++ day2/point.h
@@ -0,0 +1,170 @@
+#if !defined(POINT_T_H)
+#define POINT_T_H
+
+#include <iostream>		// cout
+#include <iomanip>		// setw and setprecision on output
+#include <string>		// std::string
+#include <vector>
+#include <tuple>
+
+#include <vector>
+#include <string>
+
+using index_t = long;
+
+/* Define a numeric only template type (C++23 concepts)
+ * Used to make sure the template for creating point_t are numeric types
+ * e.g.; integral or floating point.
+ */
+template<typename T>
+concept Numeric = std::integral<T> or std::floating_point<T>;
+
+struct point_t {
+	index_t x = 0;
+	index_t y = 0;
+	index_t z = 0;
+
+	point_t() {}
+
+	/* Use a template for constructor so I don't have to worry about
+	 * mixing int and unsigned int, size_t, etc. on creation.
+	 */
+	template <Numeric Tx, Numeric Ty>
+	point_t(Tx x, Ty y, Tx z = 0) :
+		x(static_cast<index_t>(x)), 
+		y(static_cast<index_t>(y)), 
+		z(static_cast<index_t>(z)) {
+	}
+
+	point_t(const point_t &p) :
+		x(static_cast<index_t>(p.x)), 
+		y(static_cast<index_t>(p.y)), 
+		z(static_cast<index_t>(p.z)) {
+	}
+
+	template <Numeric Tx, Numeric Ty>
+	point_t(const std::pair<Tx, Ty> &p) :
+		x(static_cast<index_t>(p.first)), 
+		y(static_cast<index_t>(p.second)), 
+		z(0) {
+	}
+
+	point_t(const std::vector<index_t> &v) {
+		if (v.size() > 0) {
+			this->x = v[0];
+			if (v.size() > 1) {
+				this->y = v[1];
+				if (v.size() > 2) {
+					this->z = v[2];
+				}
+			}
+		}
+	}
+
+	bool operator<(const point_t &rhs) const {		
+		// sorts by z, then y, then x 
+		if (z == rhs.z) {
+			if (y == rhs.y) {
+				return x < rhs.x;
+			}
+
+			return y < rhs.y;
+		}
+
+		return z < rhs.z;
+	}
+
+	bool operator==(const point_t &other) const {
+		return this->x == other.x
+			&& this->y == other.y
+			&& this->z == other.z;
+	}
+
+	bool operator!=(const point_t &other) const {
+		return !(*this == other);
+	}
+
+	point_t &operator=(const point_t &other) {
+		this->x = other.x;
+		this->y = other.y;
+		this->z = other.z;
+		return *this;
+	}
+
+	point_t &operator+=(const point_t& rhs) {
+		this->x += rhs.x;
+		this->y += rhs.y;
+		this->z += rhs.z;
+        return *this;
+    }
+ 
+    // friends defined inside class body are inline and are hidden from non-ADL lookup
+    friend point_t operator+(point_t lhs, const point_t &rhs) {
+        lhs += rhs; // reuse compound assignment
+        return lhs; // return the result by value (uses move constructor)
+	}
+};
+
+std::ostream& operator<<(std::ostream& os, const point_t &p);
+std::ostream& operator<<(std::ostream& os, const std::vector<point_t> &v);
+std::istream& operator>>(std::istream& is, point_t &p);
+
+/* hash function so can be put in unordered_map or set */
+template <>
+struct std::hash<point_t> {
+	size_t operator()(const point_t &p) const {
+		return std::hash<size_t>()((((size_t)p.z & 0xFFFF) << 32)
+								 | (((size_t)p.x & 0xFFFF) << 24) 
+								 | (((size_t)p.y & 0xFFFF)      ));
+	}
+};
+
+struct vector_t {
+	point_t p = {0, 0};
+	point_t dir = {0, 0};
+
+	vector_t(const point_t &p, const point_t &dir) 
+		: p(p), dir(dir) {
+	}
+
+	template <Numeric Tx, Numeric Ty>
+	vector_t(Tx x, Ty y, Tx dx, Ty dy)
+		: p(x, y), dir(dx, dy) {
+	}
+
+	template <Numeric Tx, Numeric Ty>
+	vector_t(Tx x, Ty y, Tx z, Tx dx, Ty dy, Tx dz) :
+		p(x, y, z), dir(dx, dy, dz) {
+	}
+
+	template <Numeric Tx, Numeric Ty>
+	vector_t(const std::pair<Tx, Ty> &p, const std::pair<Tx, Ty> &dir) :
+		p(p), dir(dir) {
+	}
+
+	bool operator<(const vector_t &rhs) const {
+		if (p == rhs.p) {
+			return dir < rhs.dir;
+		}
+
+		return p < rhs.p;
+	}
+
+	bool operator==(const vector_t &other) const {
+		return this->p == other.p && this->dir == other.dir;
+	}
+};
+
+std::ostream& operator<<(std::ostream& os, const vector_t &p);
+
+
+template <>
+struct std::hash<vector_t> {
+	size_t operator()(const vector_t &v) const {
+		return std::hash<size_t>()((((size_t)  v.p.x & 0xFFFF) << 32)   
+								 | (((size_t)  v.p.y & 0xFFFF) << 24)
+								 | (((size_t)v.dir.x & 0xFFFF) << 16)
+								 | (((size_t)v.dir.y & 0xFFFF)      ));
+	}
+};
+#endif
---
+++ day2/solution.cpp
@@ -0,0 +1,103 @@
+#include <chrono>       // high resolution timer
+#include <cstring>      // strtok, strdup
+#include <fstream>      // ifstream (reading file)
+#include <iostream>		// cout
+#include <iomanip>		// setw and setprecision on output
+#include <cassert>		// assert macro
+
+#include <vector>		// collectin
+#include <string>		// strings
+#include <ranges>		// ranges and views
+#include <algorithm>	// sort
+#include <numeric>		// max, reduce, etc.
+
+#include "point.h"
+
+using namespace std;
+
+/* verbosity level; 0 = nothing extra, 1 = more... Set by command line. */
+int verbose = 0;
+
+using data_t = vector<point_t>;
+using result_t = string;
+
+const data_t read_data(const string &filename);
+template <typename T> void print_result(T result, chrono::duration<double, milli> duration);
+
+/* Part 1 */
+const result_t part1(const data_t &data) {
+	point_t submarine{0, 0};
+	for (const auto &dir : data) {
+		submarine += dir;
+	}
+
+	auto result = submarine.x * submarine.y;
+	return to_string(result);
+}
+
+const result_t part2(const data_t &data) {
+	size_t result = data.size();
+
+	return to_string(result);
+}
+
+const data_t read_data(const string &filename) {
+	data_t data;
+
+	std::ifstream ifs(filename);
+
+	string direction;
+	size_t distance;
+	while (ifs >> direction >> distance) {
+		if (direction == "forward") {
+			data.push_back({distance, 0});
+		} else if (direction == "down") {
+			data.push_back({0, distance});
+		} else if (direction == "up") {
+			data.push_back({0, -distance});
+		}
+	}
+
+	return data;
+}
+
+template <typename T>
+void print_result(T result, chrono::duration<double, milli> duration) {
+	const int time_width = 10;
+	const int time_precision = 4;
+	const int result_width = 15;
+
+	cout << std::setw(result_width) << std::right << result;
+
+	cout << " ("
+		 << std::setw(time_width) << std::fixed << std::right 
+		 << std::setprecision(time_precision)
+		 << duration.count() << "ms)";
+	cout << endl;
+}
+
+int main(int argc, char *argv[]) {
+	const char *input_file = argv[1];
+	if (argc < 2) {
+		input_file = "test.txt";
+	}
+
+    auto start_time = chrono::high_resolution_clock::now();
+
+	auto data = read_data(input_file);
+
+	auto parse_time = chrono::high_resolution_clock::now();
+	print_result("parse", (parse_time - start_time));
+
+	result_t p1_result = part1(data);
+
+	auto p1_time = chrono::high_resolution_clock::now();
+	print_result(p1_result, (p1_time - parse_time));
+
+	result_t p2_result = part2(data);
+
+	auto p2_time = chrono::high_resolution_clock::now();
+	print_result(p2_result, (p2_time - p1_time));
+
+	print_result("total", (p2_time - start_time));
+}
