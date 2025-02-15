165ca0112b9c301902266b8aabc869979641a3b2
Stephen Houser <stephenhouser@gmail.com>
2025-02-15 17:44:51 -0500

    day 7 part 1

---
+++ day7/solution.cpp
@@ -0,0 +1,110 @@
+#include <chrono>       // high resolution timer
+#include <cstring>      // strtok, strdup
+#include <fstream>      // ifstream (reading file)
+#include <iostream>		// cout
+#include <iomanip>		// setw and setprecision on output
+#include <cassert>		// assert macro
+
+#include <vector>		// collectin
+#include <string>		// strings
+#include <ranges>		// ranges and views
+#include <algorithm>	// sort
+#include <numeric>		// max, reduce, etc.
+#include <unordered_map>
+#include <cmath>
+
+#include "split.h"
+
+using namespace std;
+
+using data_t = vector<size_t>;
+using result_t = string;
+
+const data_t read_data(const string &filename);
+template <typename T> void print_result(T result, chrono::duration<double, milli> duration);
+
+size_t move_crabs(const data_t &starting_crabs) {
+	// put crab subs into vector in order
+	vector<size_t> crabs = starting_crabs;
+	sort(crabs.begin(), crabs.end());
+
+	// compute the median -- it will be the closest to all points
+	long median = (long)crabs[crabs.size()/2];
+
+	// calculate distance of each crab to median
+	auto distance = crabs |
+			views::transform([median](const size_t crab) {
+				return (size_t)abs((long)crab - median);
+			});
+
+	// reduce (sum) to get fuel cost
+	size_t fuel_cost = reduce(distance.begin(), distance.end());
+	return fuel_cost;
+}
+
+/* Part 1 */
+const result_t part1(const data_t &starting_crabs) {
+	auto fuel_cost = move_crabs(starting_crabs);
+	return to_string(fuel_cost);
+}
+
+const result_t part2([[maybe_unused]] const data_t &starting_fish) {
+	return to_string(0);
+}
+
+const data_t read_data(const string &filename) {
+	data_t data;
+
+	std::ifstream ifs(filename);
+
+	string line;
+	while (getline(ifs, line)) {
+		if (!line.empty()) {
+			auto crabs = split_size_t(line);
+			data.insert(data.end(), crabs.begin(), crabs.end());
+		}
+	}
+
+	return data;
+}
+
+template <typename T>
+void print_result(T result, chrono::duration<double, milli> duration) {
+	const int time_width = 10;
+	const int time_precision = 4;
+	const int result_width = 15;
+
+	cout << std::setw(result_width) << std::right << result;
+
+	cout << " ("
+		 << std::setw(time_width) << std::fixed << std::right 
+		 << std::setprecision(time_precision)
+		 << duration.count() << "ms)";
+	cout << endl;
+}
+
+int main(int argc, char *argv[]) {
+	const char *input_file = argv[1];
+	if (argc < 2) {
+		input_file = "test.txt";
+	}
+
+    auto start_time = chrono::high_resolution_clock::now();
+
+	auto data = read_data(input_file);
+
+	auto parse_time = chrono::high_resolution_clock::now();
+	print_result("parse", (parse_time - start_time));
+
+	result_t p1_result = part1(data);
+
+	auto p1_time = chrono::high_resolution_clock::now();
+	print_result(p1_result, (p1_time - parse_time));
+
+	result_t p2_result = part2(data);
+
+	auto p2_time = chrono::high_resolution_clock::now();
+	print_result(p2_result, (p2_time - p1_time));
+
+	print_result("total", (p2_time - start_time));
+}
