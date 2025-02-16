d63c2f6f5f4bd62ef6e6c0e1e8c7b4dbc9e5a2fe
Stephen Houser <stephenhouser@gmail.com>
2025-02-16 12:47:15 -0500

    intermediate

--- day8/README.md
+++ day8/README.md
@@ -2,4 +2,4 @@
 
 Part 1: How many output strings have 2, 3, 4, or 8 characters. Simple count of those.
 
-Part 2:
+Part 2: Decode all the digits and sum the result.
--- day8/solution.cpp
+++ day8/solution.cpp
@@ -10,6 +10,7 @@
 #include <ranges>		// ranges and views
 #include <algorithm>	// sort
 #include <numeric>		// max, reduce, etc.
+#include <unordered_set>
 
 #include "split.h"
 
@@ -21,6 +22,103 @@ using result_t = string;
 
 const data_t read_data(const string &filename);
 template <typename T> void print_result(T result, chrono::duration<double, milli> duration);
+
+
+unordered_set<char> set_for(const string &str) {
+	unordered_set<char> chars(str.begin(), str.end());
+	return chars;
+}
+
+using pattern_t = unordered_set<char>;
+
+string difference(const string &p1, const string &p2) {
+	string result;
+	for (const char p : p1) {
+		if (!p2.contains(p)) {
+			result.push_back(p);
+		}
+	}
+
+	return result;
+}
+
+
+/* [0] is the char pattern for 0, [1] is the char pattern for 1*/
+const vector<string> decode(const vector<string> &patterns) {
+	vector<string> digit_pattern(10, "");
+	vector<string> six_seg;
+	vector<string> five_seg;
+
+	// find 1, 7, 4, and 8, the "easy ones" and queue the others
+	for (const auto &pattern : patterns) {
+		switch (pattern.size()) {
+			case 2:	// 1
+				digit_pattern[1] = pattern;
+				break;
+			case 3:	// 7
+				digit_pattern[7] = pattern;
+				break;
+			case 4:	// 4
+				digit_pattern[4] = pattern;
+				break;
+			case 5:	// 4
+				five_seg.push_back(pattern);
+				break;
+			case 6:
+				six_seg.push_back(pattern);
+				break;
+			case 7:	// 8
+				digit_pattern[8] = pattern;
+				break;
+		}
+	}
+	// 2, 3, 5
+	assert(five_seg.size() == 3);
+	for (const auto &pattern : five_seg) {
+		if (difference(digit_pattern[1], pattern).size() == 0) {
+			digit_pattern[3] = pattern;
+		} else if (difference(pattern, digit_pattern[4]).size() == 2) {
+			digit_pattern[5] = pattern;
+		} else {
+			digit_pattern[2] = pattern;
+		}
+	}
+
+	// 0, 6, 9
+	assert(six_seg.size() == 3);
+	for (const auto &pattern : six_seg) {
+		if (difference(digit_pattern[1], pattern).size() == 1) {
+			digit_pattern[6] = pattern;
+		} else if (difference(pattern, digit_pattern[4]).size() == 2) {
+			digit_pattern[9] = pattern;
+		} else {
+			digit_pattern[0] = pattern;
+		}
+	}
+
+	return digit_pattern;
+}
+
+size_t decode_digit(const string &code, const vector<string> &digit_codes) {
+	for (size_t digit = 0; digit < digit_codes.size(); digit++) {
+		if (code == digit_codes[digit]) {
+			return digit;
+		}
+	}
+
+	return 0;
+}
+
+size_t decode(const vector<string> &codes, const vector<string> &digit_codes) {
+	size_t decoded = 0;
+	for (const auto &code : codes) {
+		auto digit = decode_digit(code, digit_codes);
+		cout << "\t" << code << " = " << digit << endl;
+		decoded = (decoded * 10) + digit;
+	}
+
+	return decoded;
+}
 
 /* Part 1 */
 const result_t part1(const data_t &data) {
@@ -45,8 +143,19 @@ const result_t part1(const data_t &data) {
 	return to_string(result);
 }
 
-const result_t part2([[maybe_unused]] const data_t &data) {
-	return to_string(0);
+const result_t part2(const data_t &data) {
+	size_t result = 0;
+
+	for (const auto &[patterns, output] : data) {
+		auto digit_patterns = decode(patterns);
+		size_t intermediate = decode(output, digit_patterns);
+
+		cout << output << " => " << intermediate << endl;
+
+		result += intermediate;
+	}
+
+	return to_string(result);
 }
 
 const data_t read_data(const string &filename) {
