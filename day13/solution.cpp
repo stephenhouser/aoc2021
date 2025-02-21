c876b0e8a006cc6e3f29481c80c1a2869f625cae
Stephen Houser <stephenhouser@gmail.com>
2025-02-21 10:45:54 -0500

    day 13 prep

---
+++ day13/solution.cpp
@@ -0,0 +1,86 @@
+#include <chrono>       // high resolution timer
+#include <cstring>      // strtok, strdup
+#include <fstream>      // ifstream (reading file)
+#include <iostream>		// cout
+#include <iomanip>		// setw and setprecision on output
+#include <cassert>		// assert macro
+
+#include <vector>		// collectin
+#include <string>		// strings
+#include <ranges>		// ranges and views
+#include <algorithm>	// sort
+#include <numeric>		// max, reduce, etc.
+
+using namespace std;
+
+using data_t = vector<string>;
+using result_t = string;
+
+const data_t read_data(const string &filename);
+template <typename T> void print_result(T result, chrono::duration<double, milli> duration);
+
+
+/* Part 1 */
+const result_t part1([[maybe_unused]] const data_t &data) {
+	return to_string(data.size());
+}
+
+const result_t part2([[maybe_unused]] const data_t &data) {
+	return to_string(0);
+}
+
+const data_t read_data(const string &filename) {
+	data_t data;
+
+	std::ifstream ifs(filename);
+
+	string line;
+	while (getline(ifs, line)) {
+		if (!line.empty()) {
+			data.push_back(line);
+		}
+	}
+
+	return data;
+}
+
+template <typename T>
+void print_result(T result, chrono::duration<double, milli> duration) {
+	const int time_width = 10;
+	const int time_precision = 4;
+	const int result_width = 15;
+
+	cout << std::setw(result_width) << std::right << result;
+
+	cout << " ("
+		 << std::setw(time_width) << std::fixed << std::right 
+		 << std::setprecision(time_precision)
+		 << duration.count() << "ms)";
+	cout << endl;
+}
+
+int main(int argc, char *argv[]) {
+	const char *input_file = argv[1];
+	if (argc < 2) {
+		input_file = "test.txt";
+	}
+
+    auto start_time = chrono::high_resolution_clock::now();
+
+	auto data = read_data(input_file);
+
+	auto parse_time = chrono::high_resolution_clock::now();
+	print_result("parse", (parse_time - start_time));
+
+	result_t p1_result = part1(data);
+
+	auto p1_time = chrono::high_resolution_clock::now();
+	print_result(p1_result, (p1_time - parse_time));
+
+	result_t p2_result = part2(data);
+
+	auto p2_time = chrono::high_resolution_clock::now();
+	print_result(p2_result, (p2_time - p1_time));
+
+	print_result("total", (p2_time - start_time));
+}
