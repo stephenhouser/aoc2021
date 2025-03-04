215ca0ea16cb84985060eb086404d5c8a58161fb
Stephen Houser <stephenhouser@gmail.com>
2025-03-04 17:08:43 -0500

    update point_t and readme

--- day0/point.h
+++ day0/point.h
@@ -16,8 +16,9 @@ struct point_t {
 	dimension_t y = 0;
 	dimension_t z = 0;
 
-	value_t u = 0;
-	value_t v = 0;
+	// value_t u = 0;
+	// value_t v = 0;
+	value_t w = 0;
 
 	point_t() {}
 
@@ -33,7 +34,10 @@ struct point_t {
 	point_t(const point_t &p) :
 		x(static_cast<dimension_t>(p.x)), 
 		y(static_cast<dimension_t>(p.y)), 
-		z(static_cast<dimension_t>(p.z)) {
+		z(static_cast<dimension_t>(p.z)),
+		// u(static_cast<dimension_t>(p.u)),
+		// v(static_cast<dimension_t>(p.v)),
+		w(static_cast<dimension_t>(p.w)) {			
 	}
 
 	template <std::convertible_to<dimension_t> Tx, std::convertible_to<dimension_t> Ty>
@@ -57,16 +61,27 @@ struct point_t {
 	}
 
 	bool operator<(const point_t &rhs) const {		
+		// // sorts by z, then y, then x 
+		// if (z == rhs.z) {
+		// 	if (y == rhs.y) {
+		// 		return x < rhs.x;
+		// 	}
+
+		// 	return y < rhs.y;
+		// }
+
+		// return z < rhs.z;
+
 		// sorts by z, then y, then x 
-		if (z == rhs.z) {
+		if (x == rhs.x) {
 			if (y == rhs.y) {
-				return x < rhs.x;
+				return z < rhs.z;
 			}
 
 			return y < rhs.y;
 		}
 
-		return z < rhs.z;
+		return x < rhs.x;		
 	}
 
 	bool operator==(const point_t &other) const {
@@ -83,6 +98,9 @@ struct point_t {
 		this->x = other.x;
 		this->y = other.y;
 		this->z = other.z;
+		// this->u = other.u;
+		// this->v = other.v;
+		this->w = other.w;
 		return *this;
 	}
 
@@ -96,6 +114,19 @@ struct point_t {
 	// friends defined inside class body are inline and are hidden from non-ADL lookup
 	friend point_t operator+(point_t lhs, const point_t &rhs) {
 		lhs += rhs; // reuse compound assignment
+		return lhs; // return the result by value (uses move constructor)
+	}
+
+	point_t &operator-=(const point_t& rhs) {
+		this->x -= rhs.x;
+		this->y -= rhs.y;
+		this->z -= rhs.z;
+		return *this;
+	}
+
+	// friends defined inside class body are inline and are hidden from non-ADL lookup
+	friend point_t operator-(point_t lhs, const point_t &rhs) {
+		lhs -= rhs; // reuse compound assignment
 		return lhs; // return the result by value (uses move constructor)
 	}
 
@@ -118,6 +149,8 @@ struct point_t {
 	
 		return {split_numbers(str)};
 	}
+
+	friend struct std::formatter<point_t>;
 };
 
 std::ostream& operator<<(std::ostream& os, const point_t &p);
@@ -133,6 +166,32 @@ struct std::hash<point_t> {
 								| (((size_t)p.y & 0xFFFF)      ));
 	}
 };
+
+template <>
+struct std::formatter<point_t> {
+    constexpr auto parse(std::format_parse_context &context) {
+        return context.begin();
+    }
+
+    auto format(const point_t &p, std::format_context &context) const {
+        auto out = context.out();
+
+		std::format_to(out, "({},{}", p.x, p.y);
+		if (p.z) {
+			std::format_to(out, ",{}", p.z);
+		}
+		std::format_to(out, ")");
+
+		return out;
+    }
+};
+
+inline dimension_t manhattan_distance(const point_t &p1, const point_t &p2) {
+	dimension_t dx = p1.x > p2.x ? p1.x - p2.x : p2.x - p1.x;
+	dimension_t dy = p1.y > p2.y ? p1.y - p2.y : p2.y - p1.y;
+	dimension_t dz = p1.z > p2.z ? p1.z - p2.z : p2.z - p1.z;
+	return dx + dy + dz;
+}
 
 // read points until we hit an empty line
 std::vector<point_t> read_points(std::istream& is, void (*fn)(point_t &point, const std::string &line) = nullptr);
--- day19/README.md
+++ day19/README.md
@@ -1,28 +1,12 @@
 # Day19: Beacon Scanner
 
-3D space, align scanners in 3D space
+3D space, align scanners in 3D space. Good thing my point_t is 3d
 
-Start with distances among points and look for 12+11+10+9+8+7+6+5+4+3+2+1=67 matching distances or more. This would mean the two scanners match in at least 12 points between them.
+Start with distances among points and look for (12+(12+1))/2 = 67 matching distances or more. This means the two scanners match in at least 12 points between them.
 
-select one of the common points as an origin in first scanner, try rotations and see if second scanner points match up (should have at least 12). if so we have a transformation for s2 to map to s1's space.
+Select one of the common distances from both scanners and the points that are that distance apart. Use the first one as the origin to merge into. Try mapping (transform and rotate) all the other points in the second space into the first. If we get more than the required 12 matches, merge the points into the first (origin) space. Add the transformed origin into the new space as a "scanner".
 
-Can I then just merge the scanners and reduce the number of spaces, then keep reducing the new set until there is just one?
+common points as an origin in first scanner, try rotations and see if second scanner points match up (should have at least 12). if so we have a transformation for s2 to map to s1's space. Then just merge the scanners and reduce the number of spaces, then keep reducing the new set until there is just one. Works.
 
+Part 2: add scanners to scanner_t and track the merged transformed origins in a vector. Then just look for the max distance among these.
 
-
-
-Old thoughts
-
-Find three distances connected to 3 points across both scanners. These would define a common plane. 
-    - track points that have distance and choose a common one?
-    - find a point that has 2 common distances to the other scanner
-
-    - distance from 0,0 for 3 points that match and have the same distances between them
-
-        A---B
-        \   /
-         \ C 
-
-    - Choose 3 points randomly and keep choosing?
-
-Find 3 points that line up when rotated
